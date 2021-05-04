46bef6c13a5d1eae80d45188814597397bef5ae0
vghosthunter <852691451@qq.com>
2021-05-04 15:21:10 +0800

    remove auth in orderserver

--- is-order-api/src/main/java/com/demo/security/order/OrderController.java
+++ is-order-api/src/main/java/com/demo/security/order/OrderController.java
@@ -1,9 +1,7 @@
 package com.demo.security.order;
 
-import com.demo.security.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,11 +14,9 @@ public class OrderController {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @PostMapping
-    public OrderInfo create(@RequestBody OrderInfo orderInfo, @AuthenticationPrincipal User user) {
-        logger.info("user id is : " + user.getId());
-        logger.info("user username is : " + user.getUsername());
+    public OrderInfo create(@RequestBody OrderInfo orderInfo, @RequestHeader String username) {
+        logger.info("user username is : " + username);
 //        PriceInfo priceInfo = restTemplate.getForObject("http://localhost:9060/price/" + orderInfo.getProductId(), PriceInfo.class);
-
 //        logger.info("price is: {}", priceInfo.getPrice());
 
         return orderInfo;
